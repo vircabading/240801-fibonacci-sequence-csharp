12f5adc55cb0b1f65c96ef71924e278be5447a60
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2024-08-01 17:08:13 -0500

    create new console app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace cabading.virgilio.fibonacci.sequence;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main (string[] args)
+    {
+        Console.WriteLine("******************************************");
+        Console.WriteLine("Fibonacci Sequence");
+        Console.WriteLine("******************************************\n");
+    }
+}
